7b8905c1665513797585371b1fbafc153ebffdae
Yousef Mahrous <yousefzentral@gmail.com>
2025-06-25 18:09:04 +0300

    init

--- server/index.ts
+++ server/index.ts
@@ -1,5 +1,5 @@
 import express, { type Request, Response, NextFunction } from "express";
-import { registerRoutes } from "./routes";
+import { registerRoutes } from "./routes.js";
 import { setupVite, serveStatic, log } from "./vite";
 
 const app = express();
