68ea1d359b8b8993c46dc0b175d1febca7a1d247
Yousef Mahrous <yousefzentral@gmail.com>
2025-06-25 17:16:23 +0300

    init

--- server/vite.ts
+++ server/vite.ts
@@ -25,7 +25,7 @@ export async function setupVite(app: Express, server: Server) {
   const serverOptions = {
     middlewareMode: true,
     hmr: {server},
-    allowedHosts: true,
+    // allowedHosts: true,
   };
 
   const vite = await createViteServer({
