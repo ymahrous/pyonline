166fd57e40ec0829a840c3f9d57888f5ff0e5541
Yousef Mahrous <yousefzentral@gmail.com>
2025-06-26 15:08:49 +0300

    init

--- server/index.ts
+++ server/index.ts
@@ -54,11 +54,12 @@ app.use((req, res, next) => {
   // importantly only setup vite in development and after
   // setting up all the other routes so the catch-all route
   // doesn't interfere with the other routes
-  if (app.get("env") === "development") {
-    await setupVite(app, server);
-  } else {
-    serveStatic(app);
-  }
+  
+  // if (app.get("env") === "development") {
+  await setupVite(app, server);
+  // } else {
+  //   serveStatic(app);
+  // }
 
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
--- server/vite.ts
+++ server/vite.ts
@@ -73,18 +73,13 @@ export async function setupVite(app: Express, server: Server) {
 }
 
 export function serveStatic(app: Express) {
-  const distPath = path.resolve(
-    __dirname,
-    "..",
-    "client",
-    "index.html",
-  );
+  const distPath = path.resolve(__dirname, "public");
 
-  // if (!fs.existsSync(distPath)) {
-  //   throw new Error(
-  //     `Could not find the build directory: ${distPath}, make sure to build the client first`,
-  //   );
-  // }
+  if (!fs.existsSync(distPath)) {
+    throw new Error(
+      `Could not find the build directory: ${distPath}, make sure to build the client first`,
+    );
+  }
 
   app.use(express.static(distPath));
   // fall through to index.html if the file doesn't exist
