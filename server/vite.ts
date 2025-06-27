ac049f4cbae33a7955205c875f5b4aa6ca922dce
Yousef Mahrous <yousefzentral@gmail.com>
2025-06-27 14:44:06 +0300

    init

--- server/vite.ts
+++ server/vite.ts
@@ -73,7 +73,7 @@ export async function setupVite(app: Express, server: Server) {
 }
 
 export function serveStatic(app: Express) {
-  const distPath = path.resolve(__dirname, "public");
+  const distPath = path.resolve(__dirname, "..", "public");
 
   if (!fs.existsSync(distPath)) {
     throw new Error(
